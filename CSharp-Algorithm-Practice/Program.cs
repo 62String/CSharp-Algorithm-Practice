a8a07fee4ee4f66880b5664581913c001c3ebf83
62String <yya009@naver.com>
2026-02-08 00:25:19 +0900

    Day 0.4: array basics - sum and max value exercises

--- CSharp-Algorithm-Practice/Program.cs
+++ CSharp-Algorithm-Practice/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int [] numbers = {3, 7, 2, 9, 1, 5};
+int saveMax = numbers[0];
 
-Console.WriteLine("Hello, World!");
+for (int i = 0; i <= 5; i ++)
+{
+    if (numbers[i] > saveMax)
+    {
+        saveMax = numbers[i];
+        
+    }
+}
+Console.WriteLine(saveMax);
