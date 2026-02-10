445a5365b0181ff23548f8ad259468bf4f3675a2
yoogihyun <yya009@naver.com>
2026-02-10 22:57:54 +0900

    Complete Day 0.4: array basics (traversal, sum, max, average)

--- CSharp-Algorithm-Practice/Program.cs
+++ CSharp-Algorithm-Practice/Program.cs
@@ -1,8 +1,10 @@
 ﻿int [] numbers = {3, 7, 2, 9, 1, 5};
-int sum = 0;
+float sum = 0;
 
 for (int i = 0; i < numbers.Length; i ++)
 {
         sum = sum + numbers[i];
 }
-Console.WriteLine(sum);
+
+float average = sum / numbers.Length; 
+Console.WriteLine(average);
